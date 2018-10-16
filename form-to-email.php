5e97bd1e4003a123ee6d126c850992ca8f7517b3
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-16 15:16:12 +0100

    .

--- form-to-email.php
+++ form-to-email.php
@@ -11,13 +11,12 @@ if(isset($_POST['submit'])){
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['textarea1'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['text'];
-
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['textarea1'];
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    header('Location: index.html')
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 
