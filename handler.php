08c4d395465837b5b04ed5422428b4575231b44f
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 13:31:27 +0100

    email change

--- handler.php
+++ handler.php
@@ -11,7 +11,6 @@ if (isset($_POST['submit'])) {
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
   header("Location: index.html?mailsend");
-  alert($_POST['name']);
 }
 
 
