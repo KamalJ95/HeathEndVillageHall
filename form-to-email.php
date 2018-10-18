0faf103179b67c54227b54ce266c0dd587f20874
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 11:05:34 +0100

    git ignore included

--- form-to-email.php
+++ form-to-email.php
@@ -9,5 +9,5 @@ $recipient = "jahah_kamal@network.lilly.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . <a href='enquiries.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>;
+echo "Thank You!" . " -" . "<a href='enquiries.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
