1af02fbb2b85fa885a7272841ab4e5b8f4d4a881
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 14:59:24 +0100

    Echo added and button removed

--- handler.php
+++ handler.php
@@ -1,9 +1,13 @@
 <?php
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
+  echo $name
   $subject = $_POST['subject'];
+  echo $subject
   $mailFrom = $_POST['mail'];
+  echo $mailFrom
   $message = $_POST['message'];
+  echo $message
 
 
   $mailTo = "jahah_kamal@network.lilly.com";
