a6bc658ae48afaf298b9d3d079a35c6d59cff704
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 12:39:09 +0100

    email changed

--- handler.php
+++ handler.php
@@ -1,26 +1,18 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
-
- */
-require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
 
 
-$pp = new FormHandler();
+  $mailTo = "jahah_kamal@network.lilly.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+  mail($mailTo,$subject, $txt, $headers);
+  header("Location: enquiries.html?mailsend");
 
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('message')->maxLength(6000);
+}
 
 
-
-
-$pp->sendEmailTo('kamalxjahah@hotmail.co.uk'); // ← Your email here
-
-echo $pp->process($_POST);
+?>
