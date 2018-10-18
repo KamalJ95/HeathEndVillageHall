b2a3d272f930fda9f66032b44dbcee4b48a9871f
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 13:41:26 +0100

    request requested

--- handler.php
+++ handler.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $subject = $_POST['subject'];
-  $mailFrom = $_POST['mail'];
-  $message = $_POST['message'];
+if (isset($_REQUEST['submit'])) {
+  $name = $_REQUEST['name'];
+  $subject = $_REQUEST['subject'];
+  $mailFrom = $_REQUEST['mail'];
+  $message = $_REQUEST['message'];
 
 
-  $mailTo = "kamalxjahah@hotmail.co.uk";
+  $mailTo = "jahah_kamal@network.lilly.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
