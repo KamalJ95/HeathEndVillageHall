2978e305aba056cffc7a214c29050fd2d7338bdf
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 15:03:27 +0100

    Forgot semi-colons

--- handler.php
+++ handler.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
-  echo $name
+  echo $name;
   $subject = $_POST['subject'];
-  echo $subject
+  echo $subject;
   $mailFrom = $_POST['mail'];
-  echo $mailFrom
+  echo $mailFrom;
   $message = $_POST['message'];
-  echo $message
+  echo $message;
 
 
   $mailTo = "jahah_kamal@network.lilly.com";
