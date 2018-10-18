ec889f35585b2f4e45e3f967fb61f27377a3e04d
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 14:49:00 +0100

    echo removed

--- handler.php
+++ handler.php
@@ -1,21 +1,15 @@
 <?php
 if (isset($_POST['submit'])) {
-  echo "ham";
   $name = $_POST['name'];
-  echo "hamroll";
   $subject = $_POST['subject'];
-  echo "hyena";
   $mailFrom = $_POST['mail'];
-  echo "cute hyena";
   $message = $_POST['message'];
-  echo "doggo goes woof";
 
 
   $mailTo = "jahah_kamal@network.lilly.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
-  echo $message;
   header("Location: index.html?mailsend");
 }
 
