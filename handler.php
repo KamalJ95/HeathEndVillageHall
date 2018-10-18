12a28c5168de7e79c92ba578c6144b9871ad98fd
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 12:53:19 +0100

    index

--- handler.php
+++ handler.php
@@ -10,7 +10,7 @@ if (isset($_POST['submit'])) {
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
-  header("Location: enquiries.html?mailsend");
+  header("Location: index.html?mailsend");
 
 }
 
