6b856cc03f93169be18680c362949bc4a11cb53e
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 13:49:04 +0100

    echo message

--- handler.php
+++ handler.php
@@ -10,6 +10,7 @@ if (isset($_REQUEST['submit'])) {
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
+  echo $message
   header("Location: index.html?mailsend");
 }
 
