e6a45c34a9ddd86b840715e90c739987891bbff1
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 12:42:23 +0100

    -

--- handler.php
+++ handler.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
   $message = $_POST['message'];
 
 
-  $mailTo = "jahah_kamal@network.lilly.com";
+  $mailTo = "kamalxjahah@hotmail.co.uk";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
