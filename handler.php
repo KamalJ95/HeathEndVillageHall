89524db79c418fb16fc447099ab837d9a45ffc90
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 15:21:31 +0100

    Attempt to fix

--- handler.php
+++ handler.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST['submit'])) {
+if (isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['mail']) && isset($_POST['message'])) {
   $name = $_POST['name'];
   echo $name;
   $subject = $_POST['subject'];
@@ -15,6 +15,8 @@ if (isset($_POST['submit'])) {
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   mail($mailTo,$subject, $txt, $headers);
   header("Location: index.html?mailsend");
+} else {
+  echo "error in post";
 }
 
 
