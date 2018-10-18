e26783acdc1929f722df1ffc52d6d6169aeab8f4
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-18 13:55:45 +0100

    echo testing

--- handler.php
+++ handler.php
@@ -1,9 +1,14 @@
 <?php
 if (isset($_POST['submit'])) {
+  echo "ham";
   $name = $_POST['name'];
+  echo "hamroll";
   $subject = $_POST['subject'];
+  echo "hyena";
   $mailFrom = $_POST['mail'];
+  echo "cute hyena";
   $message = $_POST['message'];
+  echo "doggo goes woof";
 
 
   $mailTo = "jahah_kamal@network.lilly.com";
