4b4a5c94a298e765ee4474a43b54a0423d0a7a62
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-19 15:41:25 +0100

    Attempted fix

--- handler.php
+++ handler.php
@@ -1,13 +1,16 @@
 <?php
 
   $name = $_POST['name'];
+  echo "Name" + $name;
   $email = $_POST['email'];
+  echo "Email" + $email;
   $message = $_POST['message'];
+  echo "Message" + $message;
 
-  $to = 'kamalxjahah@hotmail.co.uk';
-  $subject = 'Message from Contact Form : '.$name;
+  $to = "joneilstarkie@hotmail.co.uk";
+  $subject = "Message from Contact Form : ".$name;
 
-  $headers = "From: $name\n E-mail: $email\n Message: \n $messages";
+  $headers = "From: $name\n E-mail: $email\n Message: \n $message";
 
   mail($to, $subject, $message, $headers);
   echo "Mail has been sent";
