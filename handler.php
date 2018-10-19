429d77541f05397b054c0e72c4a8ba5a100bcb48
Kamal Jahah - Network <jahah_kamal@network.lilly.com>
2018-10-19 09:46:32 +0100

    -

--- handler.php
+++ handler.php
@@ -4,11 +4,11 @@
   $email = $_POST['email'];
   $message = $_POST['message'];
 
-  $to = 'jahah_kamal@network.lilly.com';
+  $to = 'kamalxjahah@hotmail.co.uk';
   $subject = 'Message from Contact Form : '.$name;
 
   $body = "From: $name\n E-mail: $email\n Message: \n $message";
 
   mail($to, $subject, $message);
-  header("Location: index.html?mailsend");
+
 ?>
