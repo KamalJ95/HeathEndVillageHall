9ae4863485b3dd62f157dd749d4301ed95a230f4
Kamal Jahah <jahah_kamal@network.lilly.com>
2018-11-12 14:23:08 +0000

    return

--- handler.php
+++ handler.php
@@ -6,5 +6,5 @@ $recipient = "jahah_kamal@network.lilly.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+header("Location: enquiries.html");
 ?>
